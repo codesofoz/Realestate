20dee7799ff7c0c4fe6d52553a4fc30c958677ed
Osama <osama.verified@gmail.com>
2025-09-05 18:45:16 +0500

    Project setup.

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'expo-router';
+import { View } from "react-native";
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Link href="/sign-in">Sign in</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/property">Property</Link>
+      </View>
+  );
+}
